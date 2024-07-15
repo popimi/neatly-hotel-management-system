ec66feac089fadad00a4ec40747a50c4240f0e42
Jeffiros <krittiwat.c@gmail.com>
2024-07-15 21:46:41 +0700

    fix: fix navbar & comment carousel

--- client/src/components/HomePage/CustomerSays.jsx
+++ client/src/components/HomePage/CustomerSays.jsx
@@ -2,9 +2,9 @@ import CommentsCarousel from "./CustomerSaysCarousel";
 
 function CustomerSays() {
   return (
-    <section className="bg-green-100 box-border">
-      <div className="flex flex-col items-center lg:justify-center p-5 gap-5 lg:p-10 w-full h-[calc(100dvh-48px)] lg:h-[100dvh]">
-        <h1 className="flex items-start text-5xl h-fit lg:text-[5rem] font-bold text-center p-5 lg:py-5">
+    <section className="bg-green-100 box-border p-5">
+      <div className="flex flex-col items-center justify-evenly p-5 gap-5 lg:p-10 w-full h-[calc(100dvh-48px)] lg:h-[calc(100dvh-100px)]">
+        <h1 className="flex items-start text-4xl h-fit sm:text-6xl lg:text-[5rem] font-bold text-center p-5 lg:py-5">
           Our Customer Says
         </h1>
         <div className="w-full h-[60dvh] lg:h-[80dvh]">
--- client/src/pages/AuthenticatedUser.jsx
+++ client/src/pages/AuthenticatedUser.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
-import AuthenticatedNavBar from "../components/AuthenticatedNavBar";
 import { ProfilePage } from "./ProfilePage";
 import { BookingPaymentSteps } from "./Booking/BookingPaymentSteps";
 import SearchResultPage from "../pages/SearchResultPage";
+import NavBar from "../components/NavBar";
 
 const AuthenticatedUser = () => {
   return (
     <div>
-      <AuthenticatedNavBar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/users/:id" element={<ProfilePage />} />
         <Route path="/booking" element={<BookingPaymentSteps />} />
         <Route path="/searchroom" element={<SearchResultPage />} />
+        
       </Routes>
     </div>
   );
