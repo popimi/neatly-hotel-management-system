896fffc61611d33990f07bcb068d8e8422c4bd8a
Kan <kanalovefootball@gmail.com>
2024-08-07 20:50:24 +0700

    feat: create function to use in button

--- client/src/components/BookingHistory/BookingHistoryChangeDate.jsx
+++ client/src/components/BookingHistory/BookingHistoryChangeDate.jsx
@@ -1,7 +1,7 @@
 function BookingHistoryChangeDate() {
   return (
     <div className="w-[375px] h-[1000px]  flex justify-center z-50  fixed bottom-[300px] left-[3px]">
-      <div className="w-[339px] h-[320px] rounded bg-white relative top-[700px]">
+      <div className="w-[339px] h-[265px] rounded bg-white relative top-[700px]">
         <div className="w-[339px] h-[56px] border-b py-[8px] px-[16px] flex flex-row  justify-between items-center">
           <p className="font-inter font-[600px] text-[20px] leading-[30px] text-black">
             Change Date
@@ -15,9 +15,9 @@ function BookingHistoryChangeDate() {
           <p className="w-[307px] h-[96px] font-inter font-[400px] text-[16px] leading-[24px]">
             Are you sure want to change your check-in and check-out date?
           </p>
-          <br></br>
+          
 
-          <div className="w-[307px] h-[112px] flex flex-col gap-[16px]">
+          <div className="w-[307px] h-[112px] flex flex-col gap-[16px] relative bottom-[30px] ">
             <button className="w-[307px] h-[48px] py-[16px] px-[32px] gap-[10px] text-white bg-orange-600 flex items-center justify-center">
               Yes, I want to change
             </button>
