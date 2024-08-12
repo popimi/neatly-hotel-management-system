a6fea7fa6188064345fd8294fc82252361687067
Jeffiros <krittiwat.c@gmail.com>
2024-08-13 03:22:00 +0700

    minor fixes

--- server/index.mjs
+++ server/index.mjs
@@ -134,9 +134,7 @@ app.get("/management", async (req, res) => {
     result = await connectionPool.query(
       "select * from hotel_rooms order by room_id asc"
     );
-    result = await connectionPool.query(
-      "select * from hotel_rooms order by room_id asc"
-    );
+    
   } catch {
     return res.status(500).json({ message: "Room not found" });
   }
