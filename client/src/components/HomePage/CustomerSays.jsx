8debf2dc395fc75816dbb579168a21ba6e6139f1
Jeffiros <krittiwat.c@gmail.com>
2024-08-14 23:17:46 +0700

    minor fix

--- client/src/components/HomePage/CustomerSays.jsx
+++ client/src/components/HomePage/CustomerSays.jsx
@@ -2,11 +2,11 @@ import CommentsCarousel from "./CustomerSaysCarousel";
 
 function CustomerSays() {
   return (
-    <section className="bg-green-100 box-border flex flex-col justify-evenly items-center p-5 h-[calc(100dvh-48px)] lg:h-[calc(100dvh-100px)]">
+    <section className="bg-green-100 box-border flex flex-col justify-evenly items-center p-5 h-[calc(100dvh-48px)] lg:h-[calc(100dvh-100px)] overflow-hidden">
       <h1 className="text-4xl h-fit sm:text-5xl lg:text-[5rem] font-bold text-center">
         Our Customer Says
       </h1>
-      <div className="p-5 gap-5 lg:p-10 ">
+      <div className="w-full">
         <div className="w-full">
           <CommentsCarousel />
         </div>
